6673beeab0c4c03971d854b2f5de41cf7091a98f
Long Do <me@dotronglong.com>
2017-04-16 18:34:02 +0700

    Add docker

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-centos
+- A vagrant box is based on CentOS 7 with Docker and Docker Compose are installed
---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yum update -y
+yum install -y wget git yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum-config-manager --enable docker-ce-edge
+
+yum makecache fast
+yum install -y docker-ce
+
+curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" \
+ -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+usermod -aG docker vagrant
+systemctl enable docker
